a3c19f889b5023eae246dcd7e12477aab9c3b2a5
MRSA <sanggisda@gmail.com>
2024-03-13 13:48:20 +0700

    update: app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+import './App.css'
+import ShopingIcon from './assets/shopping-icon.svg'
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <>
+      <nav className="nav">
+        <img src={ShopingIcon} alt="shopping_icon" className="nav-icon" />
+        <h1 className="nav-title">Shooping List</h1>
+      </nav>
+    </>
   );
 }
 
