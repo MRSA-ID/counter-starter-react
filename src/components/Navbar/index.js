494a58e45fc399a52ccac316efbd192d9ffba1c0
MRSA <sanggisda@gmail.com>
2024-03-18 07:10:59 +0700

    update: add css module

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,11 @@
 import ShopingIcon from '../../assets/shopping-icon.svg'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <img src={ShopingIcon} alt="shopping_icon" className="nav-icon" />
-      <h1 className="nav-title">Shooping List</h1>
+    <nav className={styles.nav}>
+      <img src={ShopingIcon} alt="shopping_icon" className={styles.navIcon} />
+      <h1 className={styles.navTitle}>Shooping List</h1>
     </nav>
   )
 }
